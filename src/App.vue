0113022c97775d2fd6c6c2ca9d3aa7d73d34b52b
FOV-RGT <Q1950088436@outlook.com>
2025-03-25 21:00:22 +0800

    稳定结构

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Button } from '@/components/ui/button'
 </script>
 
 <template>
@@ -9,6 +10,7 @@
         <div class="flex-1 flex flex-col items-center justify-center space-y-8">
             <p class="text-6xl">It's MyGO!!!!!</p>
             <p class="text-4xl">即使迷茫也要前进！</p>
+            <Button variant="destructive">click</Button>
         </div>
     </div>
 </template>
