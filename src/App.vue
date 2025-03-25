1805e25e00ef011babe6bca6c7e3875a1d76d55a
FOV-RGT <Q1950088436@outlook.com>
2025-03-25 17:40:16 +0800

    激动人心的初次push，个人网站正式开建~虽然最近又搞软设又搞软设已经忙飞了，还是先开了这个期待已久的坑吧

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+    <div class="landSpace absolute top-0 left-0 w-screen h-screen flex flex-col">
+        <div class="fixed top-0 left-0 w-full h-16 bg-[#2D496C] text-white flex items-center justify-center">
+            <h1 class="text-4xl">FOV_RGT</h1>
+        </div>
+        <div class="flex-1 flex flex-col items-center justify-center space-y-8">
+            <p class="text-6xl">It's MyGO!!!!!</p>
+            <p class="text-4xl">即使迷茫也要前进！</p>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
